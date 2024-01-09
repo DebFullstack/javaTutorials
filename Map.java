254a9c4f2fbf372e8d1f6663d896e18444d96fcf
DebFullstack <126275842+DebFullstack@users.noreply.github.com>
2024-01-08 22:00:22 -0500

    Initial Files

---
+++ Map.java
@@ -0,0 +1,27 @@
+package maps;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Map {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Integer, String> firstMap = new HashMap<>();
+			firstMap.put(0,"Zero");
+			firstMap.put(1,"One");
+			firstMap.put(2,"Two");
+			firstMap.put(3,"Three");
+			firstMap.put(4,"Four");
+			
+			System.out.println(firstMap.get(1));
+		
+//		LinkedList<Integer> intergerLinkedList = new LinkedList<Integer>();
+//		intergerLinkedList.add(1);
+//		System.out.println(intergerLinkedList);
+	}
+
+	
+
+}
